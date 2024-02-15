7a23d7471dc648e38d81a2c7ed9059d931ed6c01
Divesh Adivarekar <100069544+diveshadivarekar@users.noreply.github.com>
2024-02-16 02:02:46 +0530

    prac 14.4 vertical scroll bar

--- app/src/main/java/com/example/hello2/MainActivity.java
+++ app/src/main/java/com/example/hello2/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.hello2;
-
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,17 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        GridView gridView = findViewById(R.id.gridView);
-
-        String[] buttonLabels = new String[15];
-        for (int i = 0; i < 15; i++) {
-            buttonLabels[i] = "Button " + (i + 1);
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, buttonLabels);
-
-        gridView.setAdapter(adapter);
-
     }
 }
