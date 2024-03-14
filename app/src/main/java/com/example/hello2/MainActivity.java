dcd3a926850875f0c470330ae6213dce8474d873
Divesh Adivarekar <100069544+diveshadivarekar@users.noreply.github.com>
2024-03-14 23:32:47 +0530

    prac 30.1 sending mail

--- app/src/main/java/com/example/hello2/MainActivity.java
+++ app/src/main/java/com/example/hello2/MainActivity.java
@@ -1,59 +1,39 @@
 package com.example.hello2;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Set;
-public class MainActivity extends Activity {
-    private ListView listView;
-    private BluetoothAdapter ba;
-    private Set<BluetoothDevice> pairedDevices;
+import android.widget.Button;
+import android.widget.EditText;
+public class MainActivity extends
+        AppCompatActivity {
+    private EditText mEditTextTo,mEditTextSubject,mEditTextMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ba = BluetoothAdapter.getDefaultAdapter();
-        listView = findViewById(R.id.listView);
+        mEditTextTo = findViewById(R.id.edit_text_to);
+        mEditTextSubject =findViewById(R.id.edit_text_subject);
+        mEditTextMessage =findViewById(R.id.edit_text_message);
+        Button buttonSend =findViewById(R.id.button_send);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
     }
-    @SuppressLint("MissingPermission")
-    public void turnOn(View v) {
-        if(!ba.isEnabled()) {
-            Intent intentOn = new
-                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(intentOn, 0);
-            Toast.makeText(getApplicationContext(), "Turned ON", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "Already ON", Toast.LENGTH_SHORT).show();
-        } }
-    @SuppressLint("MissingPermission")
-    public void turnOff(View v) {
-        ba.disable();
-        Toast.makeText(getApplicationContext(), "Turned OFF", Toast.LENGTH_SHORT).show();
-    }
-    @SuppressLint("MissingPermission")
-    public void getVisible(View v) {
-        Intent intentVisible = new
-                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        startActivityForResult(intentVisible, 0);
-    }
-    @SuppressLint("MissingPermission")
-    public void listDevices(View v) {
-        pairedDevices = ba.getBondedDevices();
-        ArrayList<String> list = new ArrayList<String>();
-        for(BluetoothDevice b : pairedDevices) {
-            list.add(b.getName());
-        }
-        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
+    private void sendMail() {
+        String recipientList = mEditTextTo.getText().toString();
+        String[] recipients = recipientList.split(",");
+        String subject = mEditTextSubject.getText().toString();
+        String message = mEditTextMessage.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
     }
 }
